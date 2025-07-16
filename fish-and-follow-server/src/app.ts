aaef331f41e9936963c6f6f1891b1c0d580c14eb
nh758 <7259@pm.me>
2025-07-16 14:49:34 +0700

    add a healthcheck for datadog

--- fish-and-follow-server/src/app.ts
+++ fish-and-follow-server/src/app.ts
@@ -3,6 +3,14 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+/**
+ * This is a helathcheck for container monitoring (datadog).
+ * Just needs to respond with 200. Does not require auth.
+ */
+app.get('/healthcheck', (_req, res: Response) => {
+  res.status(200).send("Ok");
+});
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express with TypeScript!');
 });
